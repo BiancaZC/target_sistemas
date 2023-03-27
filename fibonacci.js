d8779adc2f6560bc26836ab881dbafac00c723bc
unknown <biancazcarniel@gmail.com>
2023-03-27 18:44:31 -0300

    correção execício de fibonacci

--- fibonacci.js
+++ fibonacci.js
@@ -1,25 +1,21 @@
-const num = parseInt(prompt("Digite um número: "));
-
-let a = 0, b = 1;
-let pertence = false;
-
-if (num === a || num === b) {
-  pertence = true;
-}
-
-while (b < num) {
-  const temp = a + b;
-  a = b;
-  b = temp;
-
-  if (b === num) {
-    pertence = true;
-    break;
+function fibonacci(n) {
+  // Inicializa a sequência de Fibonacci com os dois primeiros valores
+  let fibonacciSeq = [0, 1];
+  
+  // Calcula os valores da sequência até o número informado
+  while (fibonacciSeq[fibonacciSeq.length - 1] < n) {
+    let nextFibonacci = fibonacciSeq[fibonacciSeq.length - 1] + fibonacciSeq[fibonacciSeq.length - 2];
+    fibonacciSeq.push(nextFibonacci);
+  }
+  
+  // Verifica se o número informado pertence à sequência de Fibonacci
+  if (fibonacciSeq.includes(n)) {
+    return `O número ${n} pertence à sequência de Fibonacci: ${fibonacciSeq.join(", ")}.`;
+  } else {
+    return `O número ${n} não pertence à sequência de Fibonacci.`;
   }
 }
 
-if (pertence) {
-  console.log(`O número ${num} pertence à sequência de Fibonacci.`);
-} else {
-  console.log(`O número ${num} não pertence à sequência de Fibonacci.`);
-}
+// Teste
+console.log(fibonacci(13)); // O número 13 pertence à sequência de Fibonacci: 0, 1, 1, 2, 3, 5, 8, 13.
+console.log(fibonacci(7)); // O número 7 não pertence à sequência de Fibonacci.
